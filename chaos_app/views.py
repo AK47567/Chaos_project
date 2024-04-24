eb6de18de27ffd70f1461e543c37a1ac3da482ed
AK47567 <100879418+AK47567@users.noreply.github.com>
2024-04-25 01:37:00 +0530

    Update views.py

--- chaos_app/views.py
+++ chaos_app/views.py
@@ -1,12 +1,16 @@
 from celery import shared_task
 import requests
 from django.http import JsonResponse
+from .models import Image
+from .serializers import ImageSerializer
 
 @shared_task
 def generate_images(prompt):
+    queryset = Image.objects.all()
+    
     prompt = 'A red flying dog'
     headers = {
-        "authorization": "Bearer Your API Key",
+        "authorization": "Bearer MY API KEY",
         "accept": "image/*"
     }
 
@@ -23,9 +27,18 @@ def generate_images(prompt):
     )
 
     if response.status_code == 200:
+        image_data = response.content
         image_filename = f"{prompt.replace(' ', '-').lower()}.webp"
+        
+        # Save the image data to the database
+        serializer = ImageSerializer(data={'prompt': prompt, 'url': image_filename})
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return JsonResponse(serializer.errors, status=400)
+        
         with open(image_filename, 'wb') as file:
-            file.write(response.content)
-        return JsonResponse(f"Image for {prompt} generated successfully", safe=False)
+            file.write(image_data)
+        return JsonResponse(f"Image for {prompt} generated successfully and saved in the database", safe=False)
     else:
         raise Exception(str(response.json()))
